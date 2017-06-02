ceafd08390589c8f9965027d1d371d9431f1dbb9
holopopo <944903520@qq.com>
2017-06-03 01:22:22 +0800

    no message

---
+++ censortext.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Administrator on 2017/6/2.
+ */
+var censoredWords = ["sad","bad","mad"]
+var customCensoredWords = [];
+function censor(inStr){
+for(idx in censoredWords){
+    inStr = inStr.replace(censoredWords(idx),"****")
+}
+for(idx in customCensoredWords){
+    inStr = inStr.replace(customCensoredWords(idx),"****")
+}
+return inStr;
+}
+
+function addCensoredWords(word) {
+    customCensoredWords.push(word)
+}
+
+function getCensoredWords() {
+    return censoredWords.concat(customCensoredWords);
+}
+exports.censor = censor;
+exports.addCensoredWords = addCensoredWords;
+exports.getCensoredWords = getCensoredWords;
